0c107d7f8a07c8791529f9f548ca462b3ceb3f20
chris <maxlongbow@gmail.com>
2022-09-16 14:30:22 +0200

    udpate

--- clean_filter_example.sh
+++ clean_filter_example.sh
@@ -1,1 +1,13 @@
 # here we need to talk about cleaning and filtering you data
+
+#########
+# cleaning the well organized files
+#########
+# the script we will be using
+/data/programs/scripts/getlog_dna_gzfastq_q20.py
+
+cd /cerberus/projects/handor/Prap_WGS_2020/sra_data/fastq_raw
+ls *gz | head -4 > fastq.gz_raw_subset
+
+# run the cleaning script on this subset of files.
+python /data/programs/scripts/getlog_dna_gzfastq_q20.py fastq.gz_raw_subset
